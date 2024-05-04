75947cf9dbe215ee1f0da54d74b1038de322d36a
sanaz <sanazzarei1995@gmail.com>
2024-05-04 12:13:49 +0200

    add profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
 import Chat from "./Chat";
+import user_img from './images/user.png'
+import axios from "axios";
+
 
 const socket = io.connect("http://localhost:3001");
 
@@ -9,20 +12,64 @@ function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [joined, setJoined] = useState(false);
+  const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [res, setRes] = useState({});
+  const handleSelectFile = (e) => setFile(e.target.files[0]);
+  const handleUpload = async () => {
+    try {
+      setLoading(true);
+      const data = new FormData();
+      data.append("my_file", file);
+      const res = await axios.post("http://localhost:3001/upload", data);
+      setRes(res.data);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  const joinRoom = () => {
+  const joinRoom = async  () => {
     if (username !== "" && room !== "") {
       socket.emit("join-room", room);
       setJoined(true);
+      await handleUpload();
     }
   };
+
+     const handleUserImageClick = () => {
+       const fileInput = document.createElement("input");
+       fileInput.type = "file";
+       fileInput.accept= "image/*";
+       fileInput.multiple="false";
+       fileInput.style.display = "none";
+       fileInput.addEventListener("change", handleSelectFile);
+       document.body.appendChild(fileInput);
+       fileInput.click();
+     };
 
   return (
     <div className="App">
       {!joined && (
         <>
-          <h3>Let's Chat</h3>
           <div className="input-group">
+            <div className="input-container">
+              <img src={user_img} alt="user image" />
+              <p onClick={handleUserImageClick}>Edit Profile</p>
+            </div>
+            <code>
+              {Object.keys(res).length > 0
+                ? Object.keys(res).map((key) => (
+                    <p className="output-item" key={key}>
+                      <span>{key}:</span>
+                      <span>
+                        {typeof res[key] === "object" ? "object" : res[key]}
+                      </span>
+                    </p>
+                  ))
+                : null}
+            </code>
             <div className="input-container">
               <label>Name:</label>
               <input
--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 const { Server } = require("socket.io");
 
 app.use(cors());
+
+app.post("/upload", upload.single("my_file"), async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
+    res.json(cldRes);
+  } catch (error) {
+    console.log(error);
+    res.send({
+      message: error.message,
+    });
+  }
+});
+
 // Define storage for uploaded files
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -13,11 +30,15 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     // Generate unique filename
-    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
   },
 });
 
-const upload = multer({ storage: storage });
+const profile_storage = new multer.memoryStorage();
+const upload = multer({ storage: profile_storage });
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -29,23 +50,39 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log(`user${socket.id} Connected`);
-  socket.on("join-room" , (data) =>{
+  socket.on("join-room", (data) => {
     socket.join(data);
     console.log(`user ${socket.id} joined  room ${data}`);
   });
-  
-  
-  socket.on("send_message" , (data)=>{
-socket.to(data.room).emit("receive_message" , data)  });
 
- socket.on("send_file", (fileData) => {
-   console.log("Received file:", fileData);
- });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("send_file", (fileData) => {
+    console.log("Received file:", fileData);
+  });
 
   socket.on("disconnect", () => {
     console.log(`user ${socket.id} Disconnected`);
   });
 });
+
+
+
+//file-uploading in Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+async function handleUpload(file) {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "image",
+  });
+  return res;
+}
+
 
 server.listen(3001, () => {
   console.log("server is running...");
